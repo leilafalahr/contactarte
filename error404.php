5bfe9eeec9874639f605f2f68815f15934fc15ab
Leila <leilafalahr@gmail.com>
2023-01-09 16:07:03 +0100

    errores

---
+++ error404.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ERROR 404</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body class="contenedor-imagen-error">
+    <img class='imagen-error' src="assets/img/errors/error404.png">
+</body>
+</html>
